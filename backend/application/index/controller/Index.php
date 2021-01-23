3159007391ea122ddc9738e05574ac52e9421bc2
Tommy131 <entwicklerKuhler3579@outlook.de>
2021-01-23 23:41:34 +0100

    Updated the css file of the display page

--- backend/application/index/controller/Index.php
+++ backend/application/index/controller/Index.php
@@ -25,18 +25,19 @@ class Index extends \backend\system\app\ControllerBase
 		 '<title>HelloWorld - OwOFrame</title>'.
 		 '<style type="text/css">'.
 			 '*{padding: 0; margin: 0;}'.
-			 'a{color: #2E5CD5; cursor: pointer; text-decoration: none}'.
-			 'a:hover{text-decoration: underline;}'.
-			 'body{background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px}'.
+			 'body{background: #eee; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px}'.
+			 'a{color: #eee; cursor: pointer; text-decoration: none}'.
+			 'a:hover{text-decoration: none; padding: 3px; border-radius: 10px; background-color: rgb(0, 127, 152); color: #fff}'.
 			 'h1{font-size: 100px; font-weight: normal; margin-bottom: 12px;}'.
 			 'p{line-height: 1.6em; font-size: 42px}'.
 		 '</style>'.
-		 '<div style="padding: 24px 48px;">'.
+		 '<div style="margin: 100px; padding: 24px 48px; background-color: rgba(93, 137, 179, 0.8); color: #fff; border-radius: 10px;">'.
 		 '	<h1>:)</h1>'.
-			 '<p> OwOFrame [<font color="blue"><b>'.APP_VERSION.'</b></font>]<br/><span style="font-size:30px">洋工总能磨出针.</span></p>'.
+			 '<p> OwOFrame [<span style="color: rgb(51, 65, 241)"><b>'.APP_VERSION.'</b></span>]<br/><span style="font-size:30px;">洋工总能磨出针.</span></p>'.
 			 '<span style="font-size:22px;">"总有一天, 你能开发出属于自己的后端框架, 尽管不起眼." -- Hanski Jay</span><br/>'.
-		 '</div>'.
-		 '<div style="padding: 24px 48px; line-height: 1.6em; font-size: 32px"><h5>Hello World! This is a example for developer to use a backend Class.</h5></div>';
+			 '<div style="margin: 20px 0; padding: 10px; background-color: rgb(0, 99, 152); border-radius: 5px; width: 320px;">GitHub Link: <a href="https://github.com/Tommy131/OwOFrame" target="_blank">Tommy131/OwOFrame</a></div>'.
+		 	'<div style="font-size: 32px;"><h5>Hello World! This is a example for developer to use a backend Class.</h5></div>'.
+		 '</div>';
 	}
 }
 ?>
