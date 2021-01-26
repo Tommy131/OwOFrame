8d26c96642560678c90bc5332e9554524857609a
Tommy131 <entwicklerKuhler3579@outlook.de>
2021-01-26 21:09:12 +0100

    Developed OwOFrame Version Checker Command

---
+++ backend/system/console/command/VersionCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+/************************************************************************
+	 _____   _          __  _____   _____   _       _____   _____  
+	/  _  \ | |        / / /  _  \ |  _  \ | |     /  _  \ /  ___| 
+	| | | | | |  __   / /  | | | | | |_| | | |     | | | | | |     
+	| | | | | | /  | / /   | | | | |  _  { | |     | | | | | |  _  
+	| |_| | | |/   |/ /    | |_| | | |_| | | |___  | |_| | | |_| | 
+	\_____/ |___/|___/     \_____/ |_____/ |_____| \_____/ \_____/ 
+	
+	* Copyright (c) 2015-2019 OwOBlog-DGMT All Rights Reserevd.
+	* Developer: HanskiJay(Teaclon)
+	* Contact: (QQ-3385815158) E-Mail: support@owoblog.com
+
+************************************************************************/
+
+namespace backend\system\console\command;
+
+class VersionCommand extends \backend\system\console\CommandBase
+{
+	public function execute(array $params) : bool
+	{
+		\OwOBootStrap\logger("Welcome to use OwOFrame :) Current version is: " . APP_VERSION);
+		return true;
+	}
+
+	public static function getAliases() : array
+	{
+		return ['v', '-v', '-ver'];
+	}
+
+	public static function getName() : string
+	{
+		return 'version';
+	}
+
+	public static function getDescription() : string
+	{
+		return 'Look the version the OwOFrame.';
+	}
+}
