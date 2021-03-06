972ca0110c427911364369d8f8adb09c08dfd67d
Tommy131 <entwicklerKuhler3579@outlook.de>
2021-03-07 00:02:24 +0100

    Updated Copyrights

--- src/owoframe/redis/RedisManager.php
+++ src/owoframe/redis/RedisManager.php
@@ -11,8 +11,8 @@
 	* Copyright (c) 2015-2019 OwOBlog-DGMT All Rights Reserevd.
 	* Developer: HanskiJay(Teaclon)
 	* Telegram: https://t.me/HanskiJay E-Mail: support@owoblog.com
-	* 本类Session数据的存储格式采用json数据格式进行封装, 并且使用base64进行编码;
-	* !注意: 使用此方法需要自行使用session_start();
+	* GitHub: https://github.com/Tommy131
+
 ************************************************************************/
 
 declare(strict_types=1);
