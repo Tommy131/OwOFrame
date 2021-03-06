2d189eccda9f42d90e2b3af55d735956dea8fa27
Tommy131 <entwicklerKuhler3579@outlook.de>
2021-03-07 00:02:50 +0100

    Added Cookie and Session Classes

---
+++ src/owoframe/http/Cookie.php
@@ -0,0 +1,146 @@
+<?php
+
+/************************************************************************
+	 _____   _          __  _____   _____   _       _____   _____  
+	/  _  \ | |        / / /  _  \ |  _  \ | |     /  _  \ /  ___| 
+	| | | | | |  __   / /  | | | | | |_| | | |     | | | | | |     
+	| | | | | | /  | / /   | | | | |  _  { | |     | | | | | |  _  
+	| |_| | | |/   |/ /    | |_| | | |_| | | |___  | |_| | | |_| | 
+	\_____/ |___/|___/     \_____/ |_____/ |_____| \_____/ \_____/ 
+	
+	* Copyright (c) 2015-2019 OwOBlog-DGMT All Rights Reserevd.
+	* Developer: HanskiJay(Teaclon)
+	* Telegram: https://t.me/HanskiJay E-Mail: support@owoblog.com
+	* GitHub: https://github.com/Tommy131
+	
+************************************************************************/
+
+declare(strict_types=1);
+namespace owoframe\http;
+
+class Cookie
+{
+	/* @string Cookie前缀 */
+	private static $_prefix = '';
+	/* @string Cookie存储路径 */
+	private static $_path = '/';
+	
+
+	/**
+	 * @method      setPrefix
+	 * @description 设置Cookie前缀
+	 * @author      HanskiJay
+	 * @doenIn      2021-03-06
+	 * @param       string      $url 地址
+	 */
+	public static function setPrefix(string $url) : void
+	{
+		self::$_prefix = md5($url);
+		$parsed = parse_url($url);
+
+		/** 在路径后面强制加上斜杠 */
+		self::$_path = $parsed['path'] . '/';
+	}
+	
+	/**
+	 * @method      getPrefix
+	 * @description 返回Cookie前缀设置
+	 * @author      HanskiJay
+	 * @doenIn      2021-03-06
+	 * @return      string
+	 */
+	public static function getPrefix() : string
+	{
+		return self::$_prefix;
+	}
+	
+	/**
+	 * @method      getPath
+	 * @description 返回Cookie路径设置
+	 * @author      HanskiJay
+	 * @doenIn      2021-03-06
+	 * @return      string
+	 */
+	public static function getPath() : string
+	{
+		return self::$_path;
+	}
+	
+	/**
+	 * @method      getAll
+	 * @description 返回所有Cookies
+	 * @author      HanskiJay
+	 * @doenIn      2021-03-06
+	 * @return      array
+	 */
+	public static function getAll() : array
+	{
+		return $_COOKIE ?? [];
+	}
+	
+	/**
+	 * @method      get
+	 * @description 获取一个Cookie
+	 * @author      HanskiJay
+	 * @doenIn      2021-03-06
+	 * @param       string      $key     键名
+	 * @param       mixed       $default 默认返回结果
+	 * @return      mixed
+	 */
+	public static function get(string $key, $default = NULL)
+	{
+		$key = self::$_prefix . $key;
+		$value = isset($_COOKIE[$key]) ? $_COOKIE[$key] : (isset($_POST[$key]) ? $_POST[$key] : $default);
+		return is_array($value) ? $default : $value;
+	}
+
+	/**
+	 * @method      has
+	 * @description 判断是否存在一个Cookie
+	 * @author      HanskiJay
+	 * @doenIn      2021-03-06
+	 * @param       string      $key 键名
+	 * @return      boolean
+	 */
+	public static function has(string $key) : bool
+	{
+		return isset($_COOKIE[$key]);
+	}
+	
+	/**
+	 * @method      set
+	 * @description 设置一个Cookie
+	 * @author      HanskiJay
+	 * @doenIn      2021-03-06
+	 * @param       string      $key    键名
+	 * @param       mixed       $value  键值
+	 * @param       integer     $expire 过期时间
+	 */
+	public static function set(string $key, $value, $expire = 0) : void
+	{
+		$key = self::$_prefix . $key;
+		setrawcookie($key, rawurlencode($value), $expire, self::$_path);
+		$_COOKIE[$key] = $value;
+	}
+	
+	/**
+	 * @method      delete
+	 * @description 删除一个Cookie
+	 * @author      HanskiJay
+	 * @doenIn      2021-03-06
+	 * @param       string      $key 键名
+	 * @return      boolean
+	 */
+	public static function delete(string $key) : bool
+	{
+		$key = self::$_prefix . $key;
+		if (!isset($_COOKIE[$key])) {
+			return false;
+		}
+		
+		setcookie($key, '', time() - 2592000, self::$_path);
+		unset($_COOKIE[$key]);
+		return true;
+	}
+}
+?>
---
+++ src/owoframe/http/Session.php
@@ -0,0 +1,149 @@
+<?php
+
+/************************************************************************
+	 _____   _          __  _____   _____   _       _____   _____  
+	/  _  \ | |        / / /  _  \ |  _  \ | |     /  _  \ /  ___| 
+	| | | | | |  __   / /  | | | | | |_| | | |     | | | | | |     
+	| | | | | | /  | / /   | | | | |  _  { | |     | | | | | |  _  
+	| |_| | | |/   |/ /    | |_| | | |_| | | |___  | |_| | | |_| | 
+	\_____/ |___/|___/     \_____/ |_____/ |_____| \_____/ \_____/ 
+	
+	* Copyright (c) 2015-2019 OwOBlog-DGMT All Rights Reserevd.
+	* Developer: HanskiJay(Teaclon)
+	* Telegram: https://t.me/HanskiJay E-Mail: support@owoblog.com
+	* GitHub: https://github.com/Tommy131
+
+************************************************************************/
+
+declare(strict_types=1);
+namespace owoframe\http;
+
+use owoframe\exception\OwOFrameException;
+use owoframe\redis\RedisConnector;
+
+class Session
+{	
+	/* @bool 重写数据 */
+	public static $rewrite = false;
+
+
+	/**
+	 * @method      has
+	 * @description 检查是否存在单个Session数据
+	 * @author      HanskiJay
+	 * @doenIn      2021-02-13
+	 * @param       string[storeKey|存储名]
+	 * @return      boolean
+	 */
+	public static function has(string $storeKey) : bool
+	{
+		return isset($_SESSION[$storeKey]);
+	}
+
+	/**
+	 * @method      has
+	 * @description 新增一个Session数据
+	 * @author      HanskiJay
+	 * @doenIn      2021-02-13
+	 * @param       string[storeKey|存储名]
+	 * @return      boolean
+	 */
+	public static function set(string $storeKey, $data) : void
+	{
+		if(!self::has($storeKey) || self::$rewrite) {
+			$_SESSION[$storeKey] = $data;
+		}
+	}
+
+	/**
+	 * @method      has
+	 * @description 获取一个Session数据
+	 * @author      HanskiJay
+	 * @doenIn      2021-02-13
+	 * @param       string[storeKey|存储名]
+	 * @return      boolean
+	 */
+	public static function get(string $storeKey, $default = null)
+	{
+		return $_SESSION[$storeKey] ?? $default;
+	}
+
+	/**
+	 * @method      has
+	 * @description 获取全部的Session数据
+	 * @author      HanskiJay
+	 * @doenIn      2021-02-13
+	 * @param       string[storeKey|存储名]
+	 * @return      boolean
+	 */
+	public static function getAll() : ?array
+	{
+		return $_SESSION ?? null;
+	}
+
+	/**
+	 * @method      has
+	 * @description 删除单个Session数据
+	 * @author      HanskiJay
+	 * @doenIn      2021-02-13
+	 * @param       string[storeKey|存储名]
+	 * @return      boolean
+	 */
+	public static function delete(string $storeKey) : void
+	{
+		if(self::has($storeKey)) {
+			unset($_SESSION[$storeKey]);
+		}
+	}
+
+	/**
+	 * @method      start
+	 * @description 启动Session
+	 * @author      HanskiJay
+	 * @doenIn      2021-02-13
+	 * @return      void
+	 */
+	public static function start() : void
+	{
+		try {
+			if(_global('redis@enable', false) && extension_loaded("redis"))
+			{
+				if(strtolower(ini_get("session.save_handler")) === "files") {
+					ini_set("session.save_handler", "redis");
+				}
+				$server = _global('redis@server', '127.0.0.1');
+				$port   = _global('redis@port', 6379);
+				$auth   = _global('redis@auth', null);
+				$auth   = ($auth !== null) ? "?auth={$auth}" : '';
+				ini_set("session.save_path", "tcp://{$server}:{$port}" . $auth);
+				
+				$connector = RedisConnector::getInstance();
+				$connector->cfg('host',     $server, true);
+				$connector->cfg('port',     $port,   true);
+				$connector->cfg('password', $auth,   true);
+				if($redis = $connector->getConnection()) {
+					$connector->forceUsePassword();
+				} else {
+					throw new OwOFrameException('Could not use Redis for Session saver!');
+				}
+			}
+			session_start();
+		} catch(\Throwable $e) {
+			throw error($e->getMessage());
+		}
+	}
+
+	/**
+	 * @method      has
+	 * @description 重置当前客户端的Session数据
+	 * @author      HanskiJay
+	 * @doenIn      2021-02-13
+	 * @param       string[storeKey|存储名]
+	 * @return      boolean
+	 */
+	public static function reset() : void
+	{
+		$_SESSION = [];
+	}
+}
+?>
