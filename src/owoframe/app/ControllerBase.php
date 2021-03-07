05614dfd6a9e30cb0cc8ebc4a77d7fdb5e25c9a3
Tommy131 <entwicklerKuhler3579@outlook.de>
2021-03-07 02:45:53 +0100

    Migrate the code (dev@v1.0.0) to the new branch (dev@v1.0.1)

---
+++ src/owoframe/app/ControllerBase.php
@@ -0,0 +1,120 @@
+<?php
+
+/************************************************************************
+	 _____   _          __  _____   _____   _       _____   _____  
+	/  _  \ | |        / / /  _  \ |  _  \ | |     /  _  \ /  ___| 
+	| | | | | |  __   / /  | | | | | |_| | | |     | | | | | |     
+	| | | | | | /  | / /   | | | | |  _  { | |     | | | | | |  _  
+	| |_| | | |/   |/ /    | |_| | | |_| | | |___  | |_| | | |_| | 
+	\_____/ |___/|___/     \_____/ |_____/ |_____| \_____/ \_____/ 
+	
+	* Copyright (c) 2015-2019 OwOBlog-DGMT All Rights Reserevd.
+	* Developer: HanskiJay(Teaclon)
+	* Telegram: https://t.me/HanskiJay E-Mail: support@owoblog.com
+	* GitHub: https://github.com/Tommy131
+	
+************************************************************************/
+
+declare(strict_types=1);
+namespace owoframe\app;
+
+use owoframe\helper\Helper;
+
+abstract class ControllerBase
+{
+	/* @AppBase 返回AppBase实例 */
+	private $app = null;
+	/* @bool Front-End开启或关闭UsedTimeDiv(Default:true) */
+	public static $showUsedTimeDiv = true;
+	/* @string 若存在该Url, Router将会在执行完对应请求方法之后跳转到该地址 */
+	public static $goto = null;
+	/* @string 若请求的Url中包含无效的请求方法, 则默认执行该方法 */
+	public static $methodNotFound_DefaultMethod = 'methodNotFound';
+
+
+	public function __construct(AppBase $app)
+	{
+		$this->app = $app;
+	}
+
+	/**
+	 * @method      methodNotFound
+	 * @description 这只是一个示例, 参考上方注释
+	 * @return      mixed
+	 * @author      HanskiJay
+	 * @doneIn      2020-10-08 22:04
+	*/
+	public function methodNotFound()
+	{
+		return 'Requested method not found!';
+	}
+
+	/**
+	 * @method      getApp
+	 * @description 返回对应的App
+	 * @description Return corresponding AppBase Object
+	 * @return      AppBase
+	 * @author      HanskiJay
+	 * @doneIn      2020-09-10 18:49
+	*/
+	final public function getApp() : AppBase
+	{
+		return $this->app;
+	}
+
+	/**
+	 * @method      getCommonPath
+	 * @description 获取静态资源目录
+	 * Author:      HanskiJay
+	 * @doneIn      2020-09-10
+	 * @param       string      $index 文件/文件夹索引
+	 * @return      string
+	*/
+	public function getCommonPath(string $index) : string
+	{
+		return __BACKEND__ . 'common' . DIRECTORY_SEPARATOR . $index . DIRECTORY_SEPARATOR;
+	}
+
+	/**
+	 * @method      getViewPath
+	 * @description 返回Views(V)显示层的路径
+	 * @description Get the path for View(V) relativly
+	 * @author      HanskiJay
+	 * @doneIn      2020-09-10 18:49
+	 * @param       string      $index      文件/文件夹索引
+	 * @param       bool        $selectMode 选择模式[True: 返回绝对路径|Return absolute path][False: 返回相对路径|Return relative path]](Default:true)
+	 * @return      null|string
+	*/
+	final public function getViewPath(string $index, bool $selectMode = true) : ?string
+	{
+		return (($selectMode) ? $this->getApp()->getAppPath() : $this->getApp()->getAppPath($selectMode)) . "view" . DIRECTORY_SEPARATOR . $index ?? null;
+	}
+
+	/**
+	 * @method      hasViewPath
+	 * @description 判断是否存在一个View(V)目录
+	 * @description Determine whether there is a Views(V) directory
+	 * @author      HanskiJay
+	 * @doneIn      2020-09-10
+	 * @param       string      $index 文件/文件夹索引
+	 * @return      boolean
+	*/
+	final public function hasViewPath(string $index) : bool
+	{
+		$index = explode("/", $index)[0] ?? $index;
+		return is_dir(self::getViewPath($index)) || is_file(self::getViewPath($index));
+	}
+
+	/**
+	 * @method      getName
+	 * @description 返回控制器类名
+	 * @author      HanskiJay
+	 * @doenIn      2021-02-09
+	 * @return      string
+	 */
+	public function getName() : string
+	{
+		return Helper::getShortClassName($this);
+	}
+}
+?>
---
+++ src/owoframe/db/DbConfig.php
@@ -0,0 +1,164 @@
+<?php
+
+/************************************************************************
+	 _____   _          __  _____   _____   _       _____   _____  
+	/  _  \ | |        / / /  _  \ |  _  \ | |     /  _  \ /  ___| 
+	| | | | | |  __   / /  | | | | | |_| | | |     | | | | | |     
+	| | | | | | /  | / /   | | | | |  _  { | |     | | | | | |  _  
+	| |_| | | |/   |/ /    | |_| | | |_| | | |___  | |_| | | |_| | 
+	\_____/ |___/|___/     \_____/ |_____/ |_____| \_____/ \_____/ 
+	
+	* Copyright (c) 2015-2019 OwOBlog-DGMT All Rights Reserevd.
+	* Developer: HanskiJay(Teaclon)
+	* Telegram: https://t.me/HanskiJay E-Mail: support@owoblog.com
+	* GitHub: https://github.com/Tommy131
+	
+************************************************************************/
+
+declare(strict_types=1);
+namespace owoframe\db;
+
+use owoframe\exception\OwOFrameException;
+use think\facade\Db;
+
+class DbConfig extends Db
+{
+	/* @array ThinkPHP-ORM 数据库配置文件 */
+	private static $dbConfig = [];
+
+	public static function init() : void
+	{
+		self::$dbConfig = 
+		[
+			'default' => _global('mysql@default', 'mysql'),
+			'connections' =>
+			[
+				_global('mysql@default', 'mysql') => 
+				[
+					// 数据库类型
+					'type'     => _global('mysql@type', 'mysql'),
+					// 主机地址
+					'hostname' => _global('mysql@hostname', '127.0.0.1'),
+					// 用户名
+					'username' => _global('mysql@username', 'root'),
+					// 密码
+					'password' => _global('mysql@password', '123456'),
+					// 数据库名
+					'database' => _global('mysql@database', 'owocloud'),
+					// 数据库编码默认采用utf8mb4
+					'charset'  => _global('mysql@charset', 'utf8mb4'),
+					// 数据库表前缀
+					'prefix'   => _global('mysql@prefix', 'owo_'),
+					// 数据库调试模式
+					'debug'    => true
+				]
+			]
+		];
+		self::setConfig(self::$dbConfig);
+	}
+
+	/**
+	 * @method      setDefault
+	 * @description 设置默认数据库连接配置
+	 * @author      HanskiJay
+	 * @doneIn      2020-09-10
+	 * @param       string      $default 配置文件标识
+	 * @return      void
+	*/
+	public static function setDefault(string $default) : void
+	{
+		if(self::hasDbConfig($default)) {
+			self::$dbConfig['default'] = $default;
+		}
+		throw new OwOFrameException("Database configuration '{$default}' doesn't exists!");
+	}
+
+	/**
+	 * @method      getDefault
+	 * @description 获取默认的配置文件
+	 * @author      HanskiJay
+	 * @doenIn      2021-01-09
+	 * @param       string      $index   键名
+	 * @param       mixed       $default 默认返回值
+	 * @return      mixed
+	 */
+	public static function getDefault(string $index, $default ='')
+	{
+		return self::$dbConfig['connections'][self::$dbConfig['default']][$index] ?? $default;
+	}
+	
+	/**
+	 * @method      getIndex
+	 * @description 获取数据库配置中的某个元素
+	 * @description Get a element from global value $dbConfig
+	 * @author      HanskiJay
+	 * @doneIn      2020-09-19 18:03
+	 * @param       string      $index   配置索引
+	 * @param       string      $default 默认返回值
+	 * @return      string
+	*/
+	public static function getIndex(string $index, string $default = '') : string
+	{
+		return self::$dbConfig[$index] ?? DbConfig::getDefault($index) ?? $default;
+	}
+
+	/**
+	 * @method      getAll
+	 * @description 获取数据库配置
+	 * @description Get global value $dbConfig
+	 * @author      HanskiJay
+	 * @doneIn      2020-09-19
+	 * @return      string
+	*/
+	public static function getAll() : array
+	{
+		return self::$dbConfig;
+	}
+
+	/**
+	 * @method      setIndex
+	 * @description 设置数据库配置某项元素的值
+	 * @description Set the value into a element from global value $dbConfig
+	 * @author      HanskiJay
+	 * @doneIn      2020-09-19
+	 * @param       string      $index 配置索引
+	 * @param       string      $value 更新值
+	 * @return      void
+	*/
+	public static function setIndex(string $index, string $value) : void
+	{
+		// if(isset(DbConfig::DEFAULT_DB_CONFIG[$index])) {
+			self::$dbConfig[$index] = $value;
+		// }
+	}
+
+	/**
+	 * @method      hasDbConfig
+	 * @description 判断是否存在某一个数据库配置文件;
+	 * @description Check if exists a database configutration
+	 * @author      HanskiJay
+	 * @doneIn      2020-09-10
+	 * @param       string      $nickName 配置文件标识
+	 * @return      boolean
+	*/
+	public static function hasDbConfig(string $nickName) : bool
+	{
+		return isset(self::$dbConfig['connections'][$nickName]);
+	}
+
+	/**
+	 * @method      addConfig
+	 * @description 组合数据库配置文件;
+	 * @description Merge the database configurations
+	 * @author      HanskiJay
+	 * @doneIn      2020-09-10
+	 * @param       string      $nickName 配置文件标识
+	 * @param       array       $dbConfig 传入的数据
+	 * @return      void
+	*/
+	public static function addConfig(string $nickName, array $dbConfig) : void
+	{
+		self::$dbConfig['connections'][$nickName] = $dbConfig;
+	}
+
+}
