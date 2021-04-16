1df749d562ad19427115ccd4d2e39cac67098a02
Tommy131 <entwicklerKuhler3579@outlook.de>
2021-04-17 00:41:51 +0200

    Added interface StandardOutput

---
+++ src/owoframe/contract/StandardOutput.php
@@ -0,0 +1,31 @@
+<?php
+
+/************************************************************************
+	 _____   _          __  _____   _____   _       _____   _____  
+	/  _  \ | |        / / /  _  \ |  _  \ | |     /  _  \ /  ___| 
+	| | | | | |  __   / /  | | | | | |_| | | |     | | | | | |     
+	| | | | | | /  | / /   | | | | |  _  { | |     | | | | | |  _  
+	| |_| | | |/   |/ /    | |_| | | |_| | | |___  | |_| | | |_| | 
+	\_____/ |___/|___/     \_____/ |_____/ |_____| \_____/ \_____/ 
+	
+	* Copyright (c) 2015-2021 OwOBlog-DGMT All Rights Reserevd.
+	* Developer: HanskiJay(Tommy131)
+	* Telegram: https://t.me/HanskiJay E-Mail: support@owoblog.com
+	* GitHub: https://github.com/Tommy131
+
+************************************************************************/
+
+declare(strict_types=1);
+namespace owoframe\contract;
+
+interface StandardOutput
+{
+	/**
+	 * @method      getOutput
+	 * @description 标准输出方法
+	 * @author      HanskiJay
+	 * @doenIn      2021-04-16
+	 * @return      string
+	 */
+	public function getOutput() : string;
+}
