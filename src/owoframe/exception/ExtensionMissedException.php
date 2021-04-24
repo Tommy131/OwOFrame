feaa705bbed0bd6403461291f8c20131074e7c4a
Tommy131 <entwicklerKuhler3579@outlook.de>
2021-04-24 23:31:38 +0200

    Added ExtensionMissedException

---
+++ src/owoframe/exception/ExtensionMissedException.php
@@ -0,0 +1,27 @@
+<?php
+
+/************************************************************************
+	 _____   _          __  _____   _____   _       _____   _____  
+	/  _  \ | |        / / /  _  \ |  _  \ | |     /  _  \ /  ___| 
+	| | | | | |  __   / /  | | | | | |_| | | |     | | | | | |     
+	| | | | | | /  | / /   | | | | |  _  { | |     | | | | | |  _  
+	| |_| | | |/   |/ /    | |_| | | |_| | | |___  | |_| | | |_| | 
+	\_____/ |___/|___/     \_____/ |_____/ |_____| \_____/ \_____/ 
+	
+	* Copyright (c) 2015-2019 OwOBlog-DGMT All Rights Reserevd.
+	* Developer: HanskiJay(Teaclon)
+	* Telegram: https://t.me/HanskiJay E-Mail: support@owoblog.com
+	* GitHub: https://github.com/Tommy131
+
+************************************************************************/
+
+declare(strict_types=1);
+namespace owoframe\exception;
+
+class ExtensionMissedException extends OwOFrameException
+{
+	public function __construct(string $name, int $code = 0, \Throwable $previous = null)
+	{
+		parent::__construct("Missed PHP Extension: '{$name}'!", $code, $previous);
+	}
+}
