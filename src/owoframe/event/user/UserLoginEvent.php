b8b613f99c4ec28d656aabfc9646985d7b46bc6f
Tommy131 <entwicklerKuhler3579@outlook.de>
2021-11-01 00:03:21 +0100

    Created UserLoginEvent.php

---
+++ src/owoframe/event/user/UserLoginEvent.php
@@ -0,0 +1,30 @@
+<?php
+
+/*********************************************************************
+	 _____   _          __  _____   _____   _       _____   _____
+	/  _  \ | |        / / /  _  \ |  _  \ | |     /  _  \ /  ___|
+	| | | | | |  __   / /  | | | | | |_| | | |     | | | | | |
+	| | | | | | /  | / /   | | | | |  _  { | |     | | | | | |  _
+	| |_| | | |/   |/ /    | |_| | | |_| | | |___  | |_| | | |_| |
+	\_____/ |___/|___/     \_____/ |_____/ |_____| \_____/ \_____/
+
+	* Copyright (c) 2015-2021 OwOBlog-DGMT.
+	* Developer: HanskiJay(Tommy131)
+	* Telegram:  https://t.me/HanskiJay
+	* E-Mail:    support@owoblog.com
+	* GitHub:    https://github.com/Tommy131
+
+**********************************************************************/
+
+declare(strict_types=1);
+namespace owoframe\event\user;
+
+use owoframe\user\UserManager;
+
+class UserLoginEvent extends \owoframe\event\Event
+{
+
+	public function __construct()
+	{
+	}
+}
